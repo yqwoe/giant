e0646a28005c7321f8b04c0f122b5a261ddbc4c2
rocLv <wangqsh999@gmail.com>
2016-12-09 09:40:02 +0800

    fix car model

--- app/controllers/api/v1/cars_controller.rb
+++ app/controllers/api/v1/cars_controller.rb
@@ -1,9 +1,12 @@
 class Api::V1::CarsController < Api::V1::BaseController
   def create
-    current_user.cars.build(car_model_id: car_model_id,
+    car_model = CarBrand.find(params[:car_brand_id]).car_models.find_by_cn_name(params[:car_model])
+    render json: {success: false, message: 'Invalid car model'} unless car_model
+
+    current_user.cars.build(car_model_id: car_model.id,
                       licensed_id: params[:licensed_id])
 
-    if @user.save!
+    if current_user.save!
       render json: { success: true }
     else
       render json: { success: false }
