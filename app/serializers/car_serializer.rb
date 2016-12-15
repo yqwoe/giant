a1f1ad12dcf2b4b5c3539cf32266ee171cb849e6
rocLv <wangqsh999@gmail.com>
2016-12-15 20:31:28 +0800

    return more car models

--- app/serializers/car_serializer.rb
+++ app/serializers/car_serializer.rb
@@ -1,7 +1,11 @@
 class CarSerializer < ActiveModel::Serializer
-  attributes :licensed_id, :joined_at, :valid_at, :visited_at, :city, :car_brand_url
+  attributes :licensed_id, :car_model, :joined_at, :valid_at, :visited_at, :city, :car_brand_url
 
   def car_brand_url
     CarBrandSerializer.new(object.car_model.car_brand)
+  end
+
+  def car_model
+    CarModelSerializer.new(object.car_model)
   end
 end
