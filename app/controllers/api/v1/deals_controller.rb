e92878abe2b4c640249abfb056546e719b40c296
rocLv <wangqsh999@gmail.com>
2017-01-05 21:16:34 +0800

    GET shop deals

--- app/controllers/api/v1/deals_controller.rb
+++ app/controllers/api/v1/deals_controller.rb
@@ -1,15 +1,10 @@
 class Api::V1::DealsController <  Api::V1::BaseController
   def index
     records = []
-    current_user.cars.each do |car|
-      car.deals.order(id: :desc).each do |deal|
-        records << {
-          title: deal.shop.name,
-          date:  deal.cleaned_at.strftime('%Y-%m-%d'),
-          time:  deal.cleaned_at.strftime('%H:%M'),
-          address: deal.shop.address
-        }
-      end
+    if current_user.member?
+      records = get_member_deals
+    elsif current_user.shop_owner?
+      records = get_shop_deals
     end
 
     render json: { record: records }
@@ -18,4 +13,43 @@ class Api::V1::DealsController <  Api::V1::BaseController
   def show
     current_user.deals.find params[:id]
   end
+
+  private
+
+    def get_member_deals
+      return nil unless current_user.member?
+
+      records = []
+      current_user.cars.each do |car|
+        car.deals.order(id: :desc).each do |deal|
+          records << {
+            title: deal.shop.name,
+            date:  deal.cleaned_at.strftime('%Y-%m-%d'),
+            time:  deal.cleaned_at.strftime('%H:%M'),
+            address: deal.shop.address
+          }
+        end
+      end
+
+      records
+    end
+
+    def get_shop_deals
+      return nil unless current_user.shop_owner?
+
+      records = []
+      current_user.shops.each do |shop|
+        shop.deals.order(id: :desc).each do |deal|
+          records << {
+            licensed_id: deal.car.licensed_id,
+            date:  deal.cleaned_at.strftime('%Y-%m-%d'),
+            time:  deal.cleaned_at.strftime('%H:%M'),
+            car_brand: deal.car&.car_model&.car_brand&.cn_name
+          }
+        end
+      end
+
+      records
+    end
+
 end
