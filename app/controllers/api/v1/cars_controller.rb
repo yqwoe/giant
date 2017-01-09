7724800b15df09ec2cbbeef080eefcf5fd3400ce
rocLv <wangqsh999@gmail.com>
2017-01-09 22:32:47 +0800

    fix car create

--- app/controllers/api/v1/cars_controller.rb
+++ app/controllers/api/v1/cars_controller.rb
@@ -1,6 +1,6 @@
 class Api::V1::CarsController < Api::V1::BaseController
   def create
-    if current_user.cars.find_by(params[:licensed_id])
+    if current_user.cars.find_by(licensed_id: params[:licensed_id])
       render json: { success: false, message: '该车牌已经被绑定' }
     end
     car_model = CarBrand.find(params[:car_brand_id]).car_models.find_by_cn_name(params[:car_model])
