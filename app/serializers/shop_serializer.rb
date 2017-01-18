70fb34cc6f5dc9f8b18a5b6067327220c381760e
rocLv <wangqsh999@gmail.com>
2017-01-18 14:32:04 +0800

    fix shop image path

--- app/serializers/shop_serializer.rb
+++ app/serializers/shop_serializer.rb
@@ -7,6 +7,6 @@ class ShopSerializer < ActiveModel::Serializer
 
 
   def img_url
-    image_path("https://autoxss.com/assets/#{object.image}")
+    image_path("https://autoxss.com/assets/shops/#{object.image}")
   end
 end
