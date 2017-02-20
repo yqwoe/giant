8d7fb0fb1627f354b568d8d7b0f50bcee0d6ae3c
rocLv <wangqsh999@gmail.com>
2017-02-20 16:11:16 +0800

    update version api

--- app/controllers/api/v1/versions_controller.rb
+++ app/controllers/api/v1/versions_controller.rb
@@ -1,9 +1,41 @@
 class Api::V1::VersionsController < ApplicationController
   def show
-    render json: {
-      currentVsersion: '2.0.3',
-      packageSize: '26M',
-      updateContent: ["1. Bug修复。", "2. 完善用户活动版块。"]
-    }
+    send "#{params[:os]}_#{params[:kind]}_version"
   end
+
+  private
+
+    def ios_car_owner_version
+      render json: {
+        currentVersion: '2.0.4',
+        packageSize: '26M',
+        updateContent: ["1. Bug修复。", "2. 完善用户活动版块。"]
+      }
+    end
+
+    def android_car_owner_version
+      render json: {
+        currentVersion: '2.0.4',
+        packageSize: '25M',
+        updateContent: ["1. Bug修复。", "2. 完善用户活动版块。"],
+        download_url: 'https://autoxss.com/apk/xishuashua.apk'
+      }
+    end
+
+    def ios_shop_version
+      render json: {
+        currentVersion: '2.0.6',
+        packageSize: '25M',
+        updateContent: ["1. Bug修复。", "2. 完善用户活动版块。"]
+      }
+    end
+
+    def android_shop_version
+      render json: {
+        currentVersion: '2.0.6',
+        packageSize: '25M',
+        updateContent: ["1. Bug修复。", "2. 完善用户活动版块。"],
+        download_url: 'https://autoxss.com/apk/xishuashua.apk'
+      }
+    end
 end
