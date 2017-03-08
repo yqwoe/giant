a2891b1e20a19cf9364aa9bc36fe51331b67b3fb
rocLv <wangqsh999@gmail.com>
2017-03-08 11:03:16 +0800

    limit dadi browser deals

--- app/controllers/admin/deals_controller.rb
+++ app/controllers/admin/deals_controller.rb
@@ -1,5 +1,13 @@
 class Admin::DealsController < Admin::BaseController
+  before_action :authenticate_admin?
+
   def index
     @deals = Deal.order(:shop_id).page(params[:page])
   end
+
+  private
+    #TODO: import cancancan gem
+    def authenticate_admin?
+      render text: '请和管理员联系！' unless current_user.admin?
+    end
 end
