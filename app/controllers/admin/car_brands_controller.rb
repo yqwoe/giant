1aef0f5f31c1e4763771841176ef241e49f99948
rocLv <wangqsh999@gmail.com>
2017-03-14 13:59:51 +0800

    add car brands edit page

--- app/controllers/admin/car_brands_controller.rb
+++ app/controllers/admin/car_brands_controller.rb
@@ -1,5 +1,16 @@
 class Admin::CarBrandsController < Admin::BaseController
+  before_action :set_car_brand, only: [:edit, :update]
   def index
-    @car_brands = CarBrand.page(params[:page])
+    @car_brands = CarBrand.all
+  end
+
+  def edit
+
+  end
+
+  private
+
+  def set_car_brand
+    @car_brand = CarBrand.find params[:id]
   end
 end
