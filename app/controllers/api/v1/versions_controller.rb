959ac9569d4983427bfb764de506635f57429dd8
silent_boy <silent_boy00@163.com>
2017-03-23 18:32:59 +0800

    提交版本更新内容

--- app/controllers/api/v1/versions_controller.rb
+++ app/controllers/api/v1/versions_controller.rb
@@ -25,17 +25,17 @@ class Api::V1::VersionsController < ApplicationController
     def ios_shop_version
       render json: {
         currentVersion: '2.0.7',
-        packageSize: '25M',
-        updateContent: ["1. Bug修复。", "2. 完善用户活动版块。"]
+        packageSize: '12M',
+        updateContent: ["1. 增加版本更新提示功能。", "2. 添加扫描二维码动画效果。", "3. 其他部分功能优化。"]
       }
     end
 
     def android_shop_version
       render json: {
         currentVersion: '2.0.7',
-        packageSize: '25M',
-        updateContent: ["1. Bug修复。", "2. 完善用户活动版块。"],
+        packageSize: '12.6M',
+        updateContent: ["1. 增加版本更新提示功能。", "2. 添加扫描二维码动画效果。", "3. 其他部分功能优化。"],
         download_url: 'https://www.autoxss.com/apk/xishuashua_shop.apk'
-      }
+      }gst
     end
 end
