46baa051c65bf560603f7113d81c139c523960b3
silent_boy <silent_boy00@163.com>
2017-04-06 10:58:54 +0800

    添加评论提交测试

--- test/controllers/api/v1/comments_controller_test.rb
+++ test/controllers/api/v1/comments_controller_test.rb
@@ -1,4 +1,32 @@
 require 'test_helper'
 
 class Api::V1::CommentsControllerTest < ActionDispatch::IntegrationTest
+    test "create comment success" do
+      @user = create(:user)
+      @deal = create(:deal)
+      @comment = create(:comment, deal_id: @deal.id)
+      post api_v1_comments_url, params:{deal_id: @deal.id,
+                                        content: @comment.content,
+                                        clean_star: 5,
+                                        service_star: 5,
+                                        env_star: 5,
+                                        user_token: @user.authentication_token}
+      @deal.save!
+      assert_response :success
+      assert_equal JSON.parse(response.body)['comment']['content'], @comment.content
+    end
+
+    test "create comment and deal id can't be nil" do
+        @user = create(:user)
+        @deal = create(:deal)
+        @comment = create(:comment, deal_id: @deal.id)
+        post api_v1_comments_url, params:{deal_id: 'invalid deal_id',
+                                        content: @comment.content,
+                                        clean_star: 5,
+                                        service_star: 5,
+                                        env_star: 5,
+                                        user_token: @user.authentication_token}
+        assert_response :success
+        assert_equal JSON.parse(response.body)['message'],'deal id can not be nil'
+    end
 end
