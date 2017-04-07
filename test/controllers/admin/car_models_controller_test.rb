5225c6c279e8d5ae17b575a1079d7cb58db5521f
silent_boy <silent_boy00@163.com>
2017-04-07 10:32:40 +0800

    解决冲突

---
+++ test/controllers/admin/car_models_controller_test.rb
@@ -0,0 +1,14 @@
+require 'test_helper'
+
+class Admin::CarModelsControllerTest < ActionDispatch::IntegrationTest
+  test "should get edit" do
+    get admin_car_models_edit_url
+    assert_response :success
+  end
+
+  test "should get update" do
+    get admin_car_models_update_url
+    assert_response :success
+  end
+
+end
