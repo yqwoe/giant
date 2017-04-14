d4c3402a1833cda8fbb823c56cf325cd176b78e2
rocLv <wangqsh999@gmail.com>
2017-04-14 10:48:30 +0800

    coupon accept parameters: tag

--- Gemfile
+++ Gemfile
@@ -125,7 +125,7 @@ group :development do
   # Spring speeds up development by keeping your application running in the background. Read more: https://github.com/rails/spring
   gem 'spring'
   gem 'spring-watcher-listen', '~> 2.0.0'
-  gem 'brakeman'
+  gem 'brakeman', require: false
   gem 'bullet'
   gem "zero_downtime_migrations"
 end
--- app/controllers/api/v1/coupons_controller.rb
+++ app/controllers/api/v1/coupons_controller.rb
@@ -1,5 +1,15 @@
 class Api::V1::CouponsController < Api::V1::BaseController
   def index
-    render json: Coupon.all
+    if params[:tag] == 'all' or  Coupon.statuses.include? params[:tag]
+      render json: coupon_result
+    else
+      render json: Coupon.count
+    end
+  end
+
+  private
+
+  def coupon_result
+    Coupon.send params[:tag]
   end
 end
