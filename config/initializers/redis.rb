3aa414a9920fab081d34a71dcee96fb2c6bf86f6
rocLv <wangqsh999@gmail.com>
2017-04-18 17:18:53 +0800

    update redis server config

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,1 +1,5 @@
-$redis = Redis::Namespace.new("giant", redis: Redis.new)
+if Rails.env.production?
+  $redis = Redis::Namespace.new("giant", redis: Redis.new(host: '10.25.76.242'))
+else
+  $redis = Redis::Namespace.new("giant", redis: Redis.new)
+end
