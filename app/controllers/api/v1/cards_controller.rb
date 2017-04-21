3db51a69fd2b33072b92998cf7f4f183d4dccff6
rocLv <wangqsh999@gmail.com>
2017-04-21 12:26:51 +0800

    HOTFIX: card ccan no active

--- app/controllers/api/v1/cards_controller.rb
+++ app/controllers/api/v1/cards_controller.rb
@@ -1,26 +1,26 @@
 class Api::V1::CardsController < Api::V1::BaseController
   def create
-    card = Card.find_by_pin(params[:card_pin])
+    @card = Card.find_by_pin(params[:card_pin])
 
-    render_invalid_card and return unless card
-    render_card_has_been_actived and return if card.actived?
+    render_invalid_card and return unless @card
+    render_card_has_been_actived and return if @card.actived?
 
     if current_user.cars.exists?
-      car = current_user.cars.find_by_licensed_id params[:licensed_id]
+      @car = current_user.cars.find_by_licensed_id params[:licensed_id]
 
-      render json: { success: false, message: '车牌无效！' } and return unless car
+      render json: { success: false, message: '车牌无效！' } and return unless @car
 
-      car.joined_at ||= Time.zone.now
-      car.valid_at = car_valid_at
-      card.transaction do
-        car.save!
-        card.actived!
-        card.actived_at = Time.zone.now
-        card.car_id = car.id
-        card.save!
+      @car.joined_at ||= Time.zone.now
+      @car.valid_at = car_valid
+      @card.transaction do
+        @car.save!
+        @card.actived!
+        @card.actived_at = Time.zone.now
+        @card.car_id = @car.id
+        @card.save!
         current_user.member!
       end
-      render json: { success: true, valid: car.valid_at }
+      render json: { success: true, valid: @car.valid_at }
     else
       render json: {success: false, message: '请先绑定车辆!'}
     end
@@ -37,6 +37,6 @@ class Api::V1::CardsController < Api::V1::BaseController
   end
 
   def car_valid
-    car.valid_at ? car.valid_at + card.range.month : card.range.month.from_now
+    @car.valid_at ? @car.valid_at + @card.range.month : @card.range.month.from_now
   end
 end
