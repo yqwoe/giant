8db72d1a3256fc17251908a62c9d4b0e55400689
rocLv <wangqsh999@gmail.com>
2017-04-27 11:37:25 +0800

    add default format to json

--- config/routes.rb
+++ config/routes.rb
@@ -36,7 +36,7 @@ Rails.application.routes.draw do
     resources :cars
   end
 
-  namespace :api do
+  namespace :api, defaults: {format: :json} do
     namespace :v1 do
       resources :authenticate
       resources :payments
