464949c48518bd93cc52fb42c5fb179fb47a042f
rocLv <wangqsh999@gmail.com>
2017-04-27 10:24:55 +0800

    add id to suite serielizer

--- app/serializers/suite_serializer.rb
+++ app/serializers/suite_serializer.rb
@@ -1,4 +1,4 @@
 class SuiteSerializer < ActiveModel::Serializer
-  attributes :name, :origin_price, :sale_price, :tags, :avatar
+  attributes :id, :name, :origin_price, :sale_price, :tags, :avatar
   has_many :wares, serializer: WareSerializer
 end
