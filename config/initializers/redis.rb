9031acfe6a0fa0a3b645e767004580f00eb11d25
rocLv <wangqsh999@gmail.com>
2017-05-19 16:50:52 +0800

    delete redis passwd temperaly

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,5 +1,5 @@
 if Rails.env.production? || Rails.env.staging?
-  $redis = Redis::Namespace.new("giant", redis: Redis.new(host: '10.25.76.242', password: ENV['REDIS_PASSWD']))
+  $redis = Redis::Namespace.new("giant", redis: Redis.new(host: '10.25.76.242'))
 else
   $redis = Redis::Namespace.new("giant", redis: Redis.new(password: ENV['REDIS_PASSWD']))
 end
