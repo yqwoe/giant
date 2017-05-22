3fa940c63d8abb94f440e53f1088a52e6d07495d
rocLv <wangqsh999@gmail.com>
2017-05-22 16:06:28 +0800

    update redis server to aliyun

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,5 +1,5 @@
 if Rails.env.production? || Rails.env.staging?
-  $redis = Redis::Namespace.new("giant", redis: Redis.new(host: '10.25.76.242'))
+  $redis = Redis::Namespace.new("giant", redis: Redis.new(host: 'r-m5e9f4cabdf6b104.redis.rds.aliyuncs.com', password: ENV['REDIS_PASSWD']))
 else
   $redis = Redis::Namespace.new("giant", redis: Redis.new(password: ENV['REDIS_PASSWD']))
 end
