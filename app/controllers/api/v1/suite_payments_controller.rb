18800b67e6f75f45eb501e82670308f24b52a22e
rocLv <wangqsh999@gmail.com>
2017-06-02 15:41:30 +0800

    fix suite payments

--- app/controllers/api/v1/suite_payments_controller.rb
+++ app/controllers/api/v1/suite_payments_controller.rb
@@ -26,12 +26,12 @@ class Api::V1::SuitePaymentsController < ActionController::API
     def convert_to_vip
       @car               = @order.car
       @car.valid_at      = @car.valid_at ? @car.valid_at + 1.year : Time.zone.now + 1.year
-      @order.finished_at = Time.zone.now
-      @order.status      = :success
-      @order.payment_gateway = :alipay
+      @suite_order.finished_at = Time.zone.now
+      @suite_order.status      = :success
+      @suite_order.payment_gateway = :alipay
 
       @car.transaction do
-        @order.save!
+        @suite_order.save!
         @car.save!
       end
     end
