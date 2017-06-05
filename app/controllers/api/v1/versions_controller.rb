cc3ffd946ba93e76bdbc0bb5793a288aed15d940
rocLv <wangqsh999@gmail.com>
2017-06-05 10:04:00 +0800

    add more description about version update

--- app/controllers/api/v1/versions_controller.rb
+++ app/controllers/api/v1/versions_controller.rb
@@ -28,6 +28,7 @@ class Api::V1::VersionsController < ApplicationController
         updateContent: ["1. 用Silentboy牌杀虫剂消灭一些影响用户体验的Bugs。",
                         "2. 增加了车行详情页面，用户可以海陆空全方面了解洗车行咯...",
                         "3. 开发了车行套餐购买的新功能，快修快保货比三家，妈妈再不说我乱花钱了 ：）",
+                        "4. 增加了跨区域显示，现在会员可以浏览其他城市的洗车行了",
                         "PS: 营销中心正在加班加点为我们尊贵的会员争取福利，那些空白的车行详情页面，我们会很快上线的哦"],
         download_url: 'https://autoxss.com/system/xishuashua_2.2.0.apk'
       }
