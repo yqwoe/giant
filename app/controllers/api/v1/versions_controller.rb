b23868ce769bbd422e38cdaa3dcf30d2c44da839
rocLv <wangqsh999@gmail.com>
2017-06-05 09:50:14 +0800

    update app version

--- app/controllers/api/v1/versions_controller.rb
+++ app/controllers/api/v1/versions_controller.rb
@@ -23,10 +23,13 @@ class Api::V1::VersionsController < ApplicationController
 
     def android_car_owner_version
       render json: {
-        currentVersion: '2.1.0',
-        packageSize: '28M',
-        updateContent: ["1. Bug修复。", "2. 完善用户活动版块。"],
-        download_url: 'https://autoxss.com/system/xishuashua_2.1.0.apk'
+        currentVersion: '2.2.0',
+        packageSize: '28.3M',
+        updateContent: ["1. 用Silentboy牌杀虫剂消灭一些影响用户体验的Bugs。",
+                        "2. 增加了车行详情页面，用户可以海陆空全方面了解洗车行咯...",
+                        "3. 开发了车行套餐购买的新功能，快修快保货比三家，妈妈再不说我乱花钱了 ：）",
+                        "PS: 营销中心正在加班加点为我们尊贵的会员争取福利，那些空白的车行详情页面，我们会很快上线的哦"],
+        download_url: 'https://autoxss.com/system/xishuashua_2.2.0.apk'
       }
     end
 
@@ -40,10 +43,10 @@ class Api::V1::VersionsController < ApplicationController
 
     def android_shop_version
       render json: {
-        currentVersion: '2.1.0',
+        currentVersion: '2.2.3',
         packageSize: '13M',
-        updateContent: ["1. 增加版本更新提示功能。", "2. 添加扫描二维码动画效果。", "3. 其他部分功能优化。"],
-        download_url: 'https://autoxss.com/system/xishuashua_shop_2.1.0.apk'
+        updateContent: ["1. 增加套餐营销记录。", "2. 添加套餐验证功能。", "3. 部分细节优化 。"],
+        download_url: 'https://autoxss.com/system/xishuashua_shop_2.2.3.apk'
       }
     end
 end
