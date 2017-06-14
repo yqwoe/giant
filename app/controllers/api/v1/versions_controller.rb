89dc6b0344c922634dfb2bea1745b8e4c3af3661
rocLv <wangqsh999@gmail.com>
2017-06-14 10:28:25 +0800

    update apk version

--- app/controllers/api/v1/versions_controller.rb
+++ app/controllers/api/v1/versions_controller.rb
@@ -23,14 +23,10 @@ class Api::V1::VersionsController < ApplicationController
 
     def android_car_owner_version
       render json: {
-        currentVersion: '2.2.0',
-        packageSize: '28.3M',
-        updateContent: ["1. 用Silentboy牌杀虫剂消灭一些影响用户体验的Bugs。",
-                        "2. 增加了车行详情页面，用户可以海陆空全方面了解洗车行咯...",
-                        "3. 开发了车行套餐购买的新功能，快修快保货比三家，妈妈再不说我乱花钱了 ：）",
-                        "4. 增加了跨区域显示，现在会员可以浏览其他城市的洗车行了",
-                        "PS: 营销中心正在加班加点为我们尊贵的会员争取福利，那些空白的车行详情页面，我们会很快上线的哦"],
-        download_url: 'https://autoxss.com/system/xishuashua_2.2.0.apk'
+        currentVersion: '2.3.0',
+        packageSize: '29.8M',
+        updateContent: ["1. 增加了联盟商家,会员可以在嘻唰唰联盟商家进行折扣消费。"],
+        download_url: 'https://autoxss.com/system/xishuashua_2.3.0.apk'
       }
     end
 
