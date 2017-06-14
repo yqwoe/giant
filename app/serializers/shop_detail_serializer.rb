1800e22c7c36adb7aaa762494b676e74d489c7ac
rocLv <wangqsh999@gmail.com>
2017-06-14 14:28:43 +0800

    Fix: useless user_id for shop_detail serializer

--- app/serializers/shop_detail_serializer.rb
+++ app/serializers/shop_detail_serializer.rb
@@ -1,7 +1,11 @@
 class ShopDetailSerializer < ShopSerializer
-  attributes :image
+  attributes :image, :user_id
 
   def image
     object.detail_images.first if object.detail_images
+  end
+
+  def user_id
+
   end
 end
