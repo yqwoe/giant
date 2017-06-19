0a95087e25bd3c39cc5c747a1843cdcddebfa2b4
rocLv <wangqsh999@gmail.com>
2017-06-19 14:31:58 +0800

    Fix jpush job

--- app/jobs/jpush_job.rb
+++ app/jobs/jpush_job.rb
@@ -2,7 +2,7 @@ class JpushJob < ApplicationJob
   queue_as :default
 
   def perform(message)
-    puts 'JPUSH job works ' + message.body
+    #puts 'JPUSH job works ' + message.body
     jpush_client = JPush::Client.new(ENV['JPUSH_APP_KEY'], ENV['JPUSH_MASTER_KEY'])
     pusher = jpush_client.pusher
 
