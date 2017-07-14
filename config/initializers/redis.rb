e7cefc47663be35d9577c048e516e95f9530e94c
rocLv <wangqsh999@gmail.com>
2017-07-14 10:13:32 +0800

    update redis server

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,5 +1,6 @@
 if Rails.env.production? || Rails.env.staging?
-  $redis = Redis::Namespace.new("giant", redis: Redis.new(host: 'r-m5e9f4cabdf6b104.redis.rds.aliyuncs.com', password: ENV['REDIS_PASSWD']))
+  REDIS_HOST = 'r-2ze17558986ad104.redis.rds.aliyuncs.com'.freeze
+  $redis = Redis::Namespace.new("giant", redis: Redis.new(host: REDIS_HOST, password: ENV['REDIS_PASSWD']))
 else
   $redis = Redis::Namespace.new("giant", redis: Redis.new(password: ENV['REDIS_PASSWD']))
 end
