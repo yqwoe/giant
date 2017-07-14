f503f847ff3d83bc30fdcb1fb9257eb9b98b6c1e
rocLv <wangqsh999@gmail.com>
2017-07-14 16:23:57 +0800

    refactor redis initializer

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,6 +1,16 @@
-if Rails.env.production? || Rails.env.staging?
-  REDIS_HOST = 'r-2ze17558986ad104.redis.rds.aliyuncs.com'.freeze
-  $redis = Redis::Namespace.new("giant", redis: Redis.new(host: REDIS_HOST, password: ENV['REDIS_PASSWD']))
-else
-  $redis = Redis::Namespace.new("giant", redis: Redis.new(password: ENV['REDIS_PASSWD']))
-end
+REDIS_HOST = case Rails.env
+             when 'production'
+               'r-2ze17558986ad104.redis.rds.aliyuncs.com'.freeze
+             when 'staging'
+               'r-m5e9f4cabdf6b104.redis.rds.aliyuncs.com'.freeze
+             else
+               'localhost'
+             end
+
+$redis = Redis::Namespace.new(
+  "giant",
+  redis: Redis.new(
+    host: REDIS_HOST,
+    password: ENV['REDIS_PASSWD']
+  )
+)
