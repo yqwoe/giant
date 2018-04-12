749407845e544fa4d494883a2e96f8a67b99ef51
yqwoe <yqwoe@live.cn>
2018-04-12 22:33:44 +0800

    修改

--- config/application.rb
+++ config/application.rb
@@ -17,10 +17,10 @@ module Giant
     WillPaginate.per_page = 30
 
     config.before_configuration do
-      env_file = File.join(Rails.root, 'config', 'env.yml')
-      YAML.load(File.open(env_file)).each do |key, value|
-        ENV[key.to_s] = value
-      end if File.exists?(env_file)
+      # env_file = File.join(Rails.root, 'config', 'env.yml')
+      # YAML.load(File.open(env_file)).each do |key, value|
+      #   ENV[key.to_s] = value
+      # end if File.exists?(env_file)
     end
 
     config.middleware.insert_before 0, Rack::Cors do
