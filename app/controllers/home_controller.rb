d814222a9d27349fbbf15fb4928bcc1ae74964d4
yqwoe <yqwoe@live.cn>
2018-06-04 04:20:38 +0800

    提交

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,12 @@
 class HomeController < ApplicationController
   def index
   #  redirect_to '/pc/pages/index.html'
+
+    puts ENV['YUNPIAN_KEY']
+    puts ENV['YUNPIAN_API']
+    puts ENV['REDIS_PASSWD']
+    puts ENV['JPUSH_APP_KEY']
+    puts ENV['JPUSH_MASTER_KEY']
+    puts ENV['RAILS_MASTER_KEY']
   end
 end
