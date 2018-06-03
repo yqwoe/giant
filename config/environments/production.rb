c2ee9c2e66bc47c1a9a56a6289bbed2ceacdf24a
yqwoe <yqwoe@live.cn>
2018-06-04 03:15:32 +0800

    修改

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -84,7 +84,7 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
-  # config.force_ssl = true
+  config.force_ssl = true
   config.public_file_server.enabled = true
 
   # config.read_encrypted_secrets = true
