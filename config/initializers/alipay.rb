45277088bf9307f0bffae2673d2af65c2f6797f8
yqwoe <yqwoe@live.cn>
2018-06-09 23:56:05 +0800

    xiuga

--- config/initializers/alipay.rb
+++ config/initializers/alipay.rb
@@ -1,2 +1,4 @@
 Alipay.app_id = '2016111802958037'
 Alipay.key =File.open(File.join Rails.root, 'config/key/rsa_private_key.pem').read
+Alipay.sign_type = 'RSA' # Available values: MD5, RSA. Default is MD5
+# Alipay.debug_mode = true # Enable parameter check. Default is true.
