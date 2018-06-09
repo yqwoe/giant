cd100e42fa423f400582b9016fd25d5d91636494
yqwoe <yqwoe@live.cn>
2018-06-09 15:25:49 +0800

    tijiao daima

--- config/initializers/alipay.rb
+++ config/initializers/alipay.rb
@@ -1,2 +1,2 @@
-Alipay.app_id = '2016111802958037'
-Alipay.key = '2016111802958037' #File.open(File.join Rails.root, 'config/key/rsa_private_key.pem').read
+Alipay.app_id = '2016032401239733'
+Alipay.key =File.open(File.join Rails.root, 'config/key/rsa_private_key.pem').read
