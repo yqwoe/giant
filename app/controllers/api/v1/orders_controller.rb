a354a348380ed6ad99017fbac91c0a41ccb60814
yqwoe <yqwoe@live.cn>
2018-06-10 09:38:38 +0800

    微调

--- app/controllers/api/v1/orders_controller.rb
+++ app/controllers/api/v1/orders_controller.rb
@@ -2,6 +2,15 @@ class Api::V1::OrdersController < Api::V1::BaseController
   before_action :set_car
   PARAMS = [:platform, :subject, :body, :total_amount]
 
+  API_URL =  'https://openapi.alipay.com/gateway.do'
+
+# setup your own credentials and certificates
+  APP_ID = '2016111802958037'
+  APP_PRIVATE_KEY=File.open(File.join Rails.root, 'config/key/private_key.pem').read
+  ALIPAY_PUBLIC_KEY=File.open(File.join Rails.root, 'config/key/public_key.pem').read
+
+# initialize a client to communicate with the Alipay API
+
   def create
     render_params_illeagal and return if params_missing?
     render_car_nil         and return unless @car
@@ -13,16 +22,35 @@ class Api::V1::OrdersController < Api::V1::BaseController
     out_trade_no = "#{platform}#{Time.zone.now.strftime('%Y%m%d%H%M%S%L')}"
     timestamp    = Time.zone.now.strftime('%Y-%m-%d+%H:%M:%S')
 
-    order = Alipay::Mobile::Service.trade_app_pay_string(
-      biz_content: {
-        out_trade_no: out_trade_no,
-        total_amount: total_amount,
-        subject:      subject,
-        # body:         body,
-      },
-      timestamp: timestamp,
-      notify_url: api_v1_payments_url
+    @alipay_client = Alipay::Client.new(
+        url: API_URL,
+        app_id: APP_ID,
+        app_private_key: APP_PRIVATE_KEY,
+        alipay_public_key: ALIPAY_PUBLIC_KEY
     )
+
+    order = @alipay_client.page_execute_url(
+        method: 'alipay.trade.app.pay',
+        biz_content: {
+            out_trade_no: out_trade_no,
+            total_amount: total_amount,
+            subject:      subject,
+            body:         body,
+        }.to_json(ascii_only: true),
+        timestamp: timestamp,
+        notify_url: api_v1_payments_url
+    )
+
+    # order = Alipay::Mobile::Service.trade_app_pay_string(
+    #   biz_content: {
+    #     out_trade_no: out_trade_no,
+    #     total_amount: total_amount,
+    #     subject:      subject,
+    #     body:         body,
+    #   },
+    #   timestamp: timestamp,
+    #   notify_url: api_v1_payments_url
+    # )
     Rails.logger.info URI.decode(order)
     # binding.pry
     # create order
